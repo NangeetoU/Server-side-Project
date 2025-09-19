dc093268732d741ff85d07400bcaf5bc3bdf3660
NineTalkative <139038742+NineTalkative@users.noreply.github.com>
2025-09-19 16:47:13 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,20 +1,26 @@
-// app.js
-
 const express = require('express');
-const app = express();
-const port = 3000;
+require('dotenv').config();
 
 // Import Route ที่เราสร้างขึ้น
+const { testConnection } = require('./config/database');
+const usersRoutes = require('./routes/usersRoutes');
 const pageRoutes = require('./routes/pageRoutes');
+
+const app = express();
+const port = 3000;
 
 // ตั้งค่า Middleware
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
-// 👇 เพิ่มบรรทัดนี้เพื่อให้ Express อ่านข้อมูลจากฟอร์มได้
 app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Mount routes
 app.use('/', pageRoutes);
+app.use('/users', usersRoutes);
 
 // เริ่มทำงานเซิร์ฟเวอร์
 app.listen(port, () => {
     console.log(`Server is running at http://localhost:${port}`);
+    testConnection();
 });
